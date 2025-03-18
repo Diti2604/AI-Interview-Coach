b3008fcc6ef58d69267524244853628e36382ef3
Indrit <ditiboyboy@gmail.com>
2025-03-18 14:28:04 +0100

    Completed the setup of frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/hello") // Adjust if using FastAPI
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Vite + Python</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
